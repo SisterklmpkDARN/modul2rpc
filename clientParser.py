fb0927fdd7a27f243d8a2d3ff64df58c03a591ab
Nabilla <nabilla@nabilla-LIFEBOOK-BH531.(none)>
2014-04-02 16:38:41 +0700

    second commit

--- clientParser.py
+++ clientParser.py
@@ -1,5 +1,7 @@
 import xmlrpclib
 
+
+
 print "Pilih perkiraan cuaca dibawah ini :"
 print "1. Cuaca Dunia1"
 print "2. Cuaca Indonesia1"
@@ -7,23 +9,25 @@ print "3. Cuaca Jabodetabek1"
 print "4. Cuaca Harian"
 print "5. Cuaca Wisata"
 print "6. Exit"
-#pilih = raw_input("Pilihanmu : ").strip()
+pilih = raw_input("Pilihanmu : ").strip()
 #print "Pilihanmu : %s" % (pilih)
 
-
 proxy = xmlrpclib.ServerProxy("http://localhost:8000")
-print proxy.mulaiTgl()
-print proxy.sampaiTgl()
-print "***Perkiraan Cuaca***"
-feeds = proxy.getCuaca()
-#print feeds
-i = 0
-for feed in feeds['item']:
-	print "Kota : %s" % feed['kota']
-	print "Cuaca : %s" % feed['cuacaKota']
-	print "Suhu Min : %s" % feed['suhumin']
-	print "Suhu Max : %s" % feed['suhumax']
-	print "Kelembapan Min : %s" % feed['kelMin']
-	print "Kelembapan Max : %s" % feed['kelMax']
-	print "---------------------------------"
-	i+=1
+feeds = proxy.getCuaca1()
+if pilih=='1':
+	print "***Perkiraan Cuaca Indonesia***"
+	print "Tanggal Mulai : %s" % feeds['mulai']
+	print "Tanggal Sampai : %s" % feeds['sampai']
+	i = 0
+	for feed in feeds['item']:
+		print "Kota : %s" % feed['kota']
+		print "Cuaca : %s" % feed['cuacaKota']
+		print "Suhu Min : %s" % feed['suhumin']
+		print "Suhu Max : %s" % feed['suhumax']
+		print "Kelembapan Min : %s" % feed['kelMin']
+		print "Kelembapan Max : %s" % feed['kelMax']
+		print "---------------------------------"
+		i+=1
+		
+if pilih=='2':
+	print proxy.coba()
--- serverParser.py
+++ serverParser.py
@@ -5,32 +5,33 @@ import xmlrpclib
 
 
 #------------------------------------------------------------------
-file1 = urllib2.urlopen('http://data.bmkg.go.id/cuaca_indo_1.xml')
-data1 = file1.read()
-file1.close()
+def getCuaca1():
+	file1 = urllib2.urlopen('http://data.bmkg.go.id/cuaca_indo_1.xml')
+	data1 = file1.read()
+	file1.close()
 
-dom1 = parseString(data1)
+	dom1 = parseString(data1)
 
-cuaca = dom1.documentElement
-#get all Tanggal
-tanggals = cuaca.getElementsByTagName("Tanggal")
-#print detil tanggal
-xml = {}
-def getCuaca():
+	cuaca = dom1.documentElement
+	#get all Tanggal
+	tanggals = cuaca.getElementsByTagName("Tanggal")
+	#print detil tanggal
+	xml = {}
+	#def getCuaca():
 	for tanggal in tanggals:		
 		print "****Tanggal****"
 		mulai = tanggal.getElementsByTagName('Mulai')[0]
 		print "Mulai = %s" % mulai.childNodes[0].data
 		xml['mulai'] = mulai.childNodes[0].data
 		sampai = tanggal.getElementsByTagName('Sampai')[0]
-		print "Selesai = %s" % sampai.childNodes[0].data
+		print "Sampai = %s" % sampai.childNodes[0].data
 		xml['sampai'] = sampai.childNodes[0].data
-			
+				
 		isis = cuaca.getElementsByTagName("Isi")
 			
 		for isi in isis:
 			rows = isi.getElementsByTagName("Row")
-				
+					
 			collections = []
 			i = 0
 			for row in rows[:]:
@@ -57,15 +58,17 @@ def getCuaca():
 				collections.append(dataItem)
 				i+=1
 				xml['item'] = collections
-			return xml
-
+	return xml	
+def coba():
+	x = 1 + 2
+	return x
+	
 server = SimpleXMLRPCServer(("localhost", 8000))
 print "Listening on  port 8000.."
 #server.register_inrospection_functions()
 server.register_multicall_functions()
-#server.register_function(mulaiTgl, 'mulaiTgl')
-#server.register_function(sampaiTgl, 'sampaiTgl')
-server.register_function(getCuaca, 'getCuaca')
+server.register_function(getCuaca1, 'getCuaca1')
+server.register_function(coba, 'coba')
 server.serve_forever()
 #print data
 #------------------------------------------------------------------------
