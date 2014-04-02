8225c69da989366abdc42fd70759b04bb66192a0
Nabilla <nabilla@nabilla-LIFEBOOK-BH531.(none)>
2014-04-02 18:39:12 +0700

    second commit

--- clientParser.py
+++ clientParser.py
@@ -3,18 +3,17 @@ import xmlrpclib
 
 
 print "Pilih perkiraan cuaca dibawah ini :"
-print "1. Cuaca Dunia1"
-print "2. Cuaca Indonesia1"
+print "1. Cuaca Indonesia"
+print "2. Cuaca Dunia"
 print "3. Cuaca Jabodetabek1"
 print "4. Cuaca Harian"
 print "5. Cuaca Wisata"
 print "6. Exit"
 pilih = raw_input("Pilihanmu : ").strip()
-#print "Pilihanmu : %s" % (pilih)
 
 proxy = xmlrpclib.ServerProxy("http://localhost:8000")
-feeds = proxy.getCuaca1()
 if pilih=='1':
+	feeds = proxy.getCuaca1()
 	print "***Perkiraan Cuaca Indonesia***"
 	print "Tanggal Mulai : %s" % feeds['mulai']
 	print "Tanggal Sampai : %s" % feeds['sampai']
@@ -28,6 +27,43 @@ if pilih=='1':
 		print "Kelembapan Max : %s" % feed['kelMax']
 		print "---------------------------------"
 		i+=1
+	#print "Peringatan : %s" % feeds['peringatan']
 		
 if pilih=='2':
-	print proxy.coba()
+	feeds = proxy.getCuaca2()
+	print "***Perkiraan Cuaca Dunia***"
+	print "Tanggal : %s" % feeds['tgl']
+	i = 0
+	for feed in feeds['item']:
+		print "Kota : %s" % feed['kota']
+		print "Cuaca : %s" % feed['cuacaKota']
+		print "Suhu Min : %s" % feed['suhumin']
+		print "Suhu Max : %s" % feed['suhumax']
+		print "---------------------------------"
+		i+=1
+	#print "Peringatan : %s" % feeds['peringatan']
+	
+if pilih=='3':
+	feeds = proxy.getCuaca3()
+	print "***Perkiraan Cuaca Wisata***"
+	print "Tanggal Mulai: %s" % feeds['dateStart']
+	print "Waktu Mulai: %s" % feeds['timeStart']
+	print "Tanggal Sampai: %s" % feeds['dateEnd']
+	print "Waktu Sampai: %s" % feeds['timeEnd']
+	print "*****Lokasi*****"
+	print "Cuaca Bali : %s" % feeds['baliWeather']
+	print "Arah Angin : %s" % feeds['windDirect']
+	print "Kecepatan Angin : %s" % feeds['windSpeed']
+	print "Waktu Sunrise : %s" % feeds['sunrise']
+	print "Waktu Sunset : %s" % feeds['sunset']
+	print "****Daerah Bali****"
+	i = 0
+	for feed in feeds['item']:
+		print "Wilayah : %s" % feed['area']
+		print "Cuaca : %s" % feed['weather']
+		print "Simbol Cuaca : %s" % feed['weatherSym']
+		print "Suhu : %s" % feed['temp']
+		print "Kelembapan udara : %s" % feed['humidity']
+		print "Ketinggian Gelombang : %s" % feed['wave']
+		print "---------------------------------"
+		i+=1
--- serverParser.py
+++ serverParser.py
@@ -30,8 +30,7 @@ def getCuaca1():
 		isis = cuaca.getElementsByTagName("Isi")
 			
 		for isi in isis:
-			rows = isi.getElementsByTagName("Row")
-					
+			rows = isi.getElementsByTagName("Row")	
 			collections = []
 			i = 0
 			for row in rows[:]:
@@ -58,17 +57,124 @@ def getCuaca1():
 				collections.append(dataItem)
 				i+=1
 				xml['item'] = collections
+				
+			#peringatan = isi.getElementsByTagName("Peringatan")
+			#xml['peringatan'] = peringatan.childNodes[0].data
 	return xml	
-def coba():
-	x = 1 + 2
-	return x
+	
+def getCuaca2():
+	file1 = urllib2.urlopen('http://data.bmkg.go.id/cuaca_dunia_1.xml')
+	data1 = file1.read()
+	file1.close()
+
+	dom1 = parseString(data1)
+
+	cuaca = dom1.documentElement
+	
+	xml = {}
+	
+	#get all Tanggal
+	tanggal = cuaca.getElementsByTagName("Tanggal")[0]
+	xml['tgl'] = tanggal.childNodes[0].data
+	#print detil tanggal
+		
+	isis = cuaca.getElementsByTagName("Isi")
+			
+	for isi in isis:
+		rows = isi.getElementsByTagName("Row")
+		collections = []
+		i = 0
+		for row in rows[:]:
+			dataItem = {}
+			print "-------------------"
+			kota = row.getElementsByTagName('Kota')[0]
+			dataItem['kota'] = kota.childNodes[0].data
+			cuacaKota = row.getElementsByTagName('Cuaca')[0]
+			dataItem['cuacaKota'] = cuacaKota.childNodes[0].data
+			suhumin = row.getElementsByTagName('SuhuMin')[0]
+			dataItem['suhumin'] = suhumin.childNodes[0].data
+			#print "Suhu Minimum : %s" % suhumin.childNodes[0].data
+			suhumax = row.getElementsByTagName('SuhuMax')[0]
+			dataItem['suhumax'] = suhumax.childNodes[0].data
+			collections.append(dataItem)
+			i+=1
+			xml['item'] = collections
+			
+		#peringatan = isi.getElementsByTagName("Peringatan")
+		#xml['peringatan'] = peringatan.childNodes[0].data
+	return xml	
+	
+def getCuaca3():
+	file1 = urllib2.urlopen('http://data.bmkg.go.id/cuaca_wisata.xml')
+	data1 = file1.read()
+	file1.close()
+
+	dom1 = parseString(data1)
+
+	travelWeather = dom1.documentElement
+	
+	xml = {}
+	
+	#get all Tanggal
+	date = travelWeather.getElementsByTagName("Date")[0]
+	#for tgl in dates:
+	valStart = date.getElementsByTagName("ValidStart")[0]
+	xml['dateStart'] = valStart.childNodes[0].data
+	valTime= date.getElementsByTagName("ValidTimeStart")[0]
+	xml['timeStart'] = valTime.childNodes[0].data
+	valEnd = date.getElementsByTagName("ValidEnd")[0]
+	xml['dateEnd'] = valEnd.childNodes[0].data
+	valTimeEnd = date.getElementsByTagName("ValidTimeEnd")[0]
+	xml['timeEnd'] = valTimeEnd.childNodes[0].data
+	#print detil tanggal
+		
+	forecasts = travelWeather.getElementsByTagName("Forecast")
+			
+	for forecast in forecasts:
+		generals = forecast.getElementsByTagName("General")
+		for general in generals:
+			baliWeather = general.getElementsByTagName("BaliWeather")[0]
+			xml['baliWeather'] = baliWeather.childNodes[0].data
+			windDirect= general.getElementsByTagName("WindDirection")[0]
+			xml['windDirect'] = windDirect.childNodes[0].data
+			windSpeed = general.getElementsByTagName("WindSpeed")[0]
+			xml['windSpeed'] = windSpeed.childNodes[0].data
+			sunrise = general.getElementsByTagName("Sunrise")[0]
+			xml['sunrise'] = sunrise.childNodes[0].data
+			sunset = general.getElementsByTagName("Sunset")[0]
+			xml['sunset'] = sunset.childNodes[0].data
+				
+		rows = forecast.getElementsByTagName("Row")
+		collections = []
+		i = 0
+		for row in rows[:]:
+			dataItem = {}
+			print "-------------------"
+			area = row.getElementsByTagName('Area')[0]
+			dataItem['area'] = area.childNodes[0].data
+			weather = row.getElementsByTagName('Weather')[0]
+			dataItem['weather'] = weather.childNodes[0].data
+			weatherSym = row.getElementsByTagName('WeatherSymbol')[0]
+			dataItem['weatherSym'] = weatherSym.childNodes[0].data
+			temp = row.getElementsByTagName('Temperature')[0]
+			dataItem['temp'] = temp.childNodes[0].data
+			humidity = row.getElementsByTagName('Humidity')[0]
+			dataItem['humidity'] = humidity.childNodes[0].data
+			wave = row.getElementsByTagName('WaveHeight')[0]
+			dataItem['wave'] = wave.childNodes[0].data
+			collections.append(dataItem)
+			i+=1
+			xml['item'] = collections
+			
+	return xml	
 	
 server = SimpleXMLRPCServer(("localhost", 8000))
 print "Listening on  port 8000.."
 #server.register_inrospection_functions()
 server.register_multicall_functions()
 server.register_function(getCuaca1, 'getCuaca1')
-server.register_function(coba, 'coba')
+server.register_function(getCuaca2, 'getCuaca2')
+server.register_function(getCuaca3, 'getCuaca3')
 server.serve_forever()
 #print data
 #------------------------------------------------------------------------
